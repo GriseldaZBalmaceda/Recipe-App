e417dd5b8f3ac4f8b947bd843b7f67f9eed39a9f
Griselda Balmaceda <gzbalmaceda@my365.bellevue.edu>
2019-09-10 15:46:04 -0500

    first commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+recipes:Recipe[]=[
+  new Recipe('Spaghetti','Yummy spaghetti!','https://cdn.apartmenttherapy.info/image/fetch/f_auto,q_auto:eco/https%3A%2F%2Fstorage.googleapis.com%2Fgen-atmedia%2F3%2F2018%2F12%2F39ee03d747ff0e93c946bdaaf07320e7679f0287.jpeg')
+]
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
