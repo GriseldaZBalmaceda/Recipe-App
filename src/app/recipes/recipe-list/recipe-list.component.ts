1b38f2722b7e2d2541dc86cb1fa270c2dd4a14f4
Griselda Balmaceda <gzbalmaceda@my365.bellevue.edu>
2019-09-11 10:13:06 -0500

    one more commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,7 @@ import {Recipe} from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 recipes:Recipe[]=[
-  new Recipe('Spaghetti','Yummy spaghetti!','https://cdn.apartmenttherapy.info/image/fetch/f_auto,q_auto:eco/https%3A%2F%2Fstorage.googleapis.com%2Fgen-atmedia%2F3%2F2018%2F12%2F39ee03d747ff0e93c946bdaaf07320e7679f0287.jpeg')
+  new Recipe('Spaghetti','Yummy spaghetti!!','https://cdn.apartmenttherapy.info/image/fetch/f_auto,q_auto:eco/https%3A%2F%2Fstorage.googleapis.com%2Fgen-atmedia%2F3%2F2018%2F12%2F39ee03d747ff0e93c946bdaaf07320e7679f0287.jpeg')
 ]
   constructor() { }
 
